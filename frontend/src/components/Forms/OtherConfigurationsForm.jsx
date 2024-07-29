208e07f1a6e1de89adf78648797434416a99bd63
davidwis <davidwis000@gmail.com>
2024-07-29 17:15:16 -0300

    Change notifications enabled without useForm

--- frontend/src/components/Forms/OtherConfigurationsForm.jsx
+++ frontend/src/components/Forms/OtherConfigurationsForm.jsx
@@ -1,24 +1,23 @@
 import { useTranslation } from 'react-i18next';
 
-import { useForm, useUserData } from '../../hooks/index';
+import { useUserData } from '../../hooks/index';
 import { useState } from 'react';
 
 const OtherConfigurationsForm = ({ user }) => {
   const { t } = useTranslation();
   const { updateUser } = useUserData();
-  const [notificationsEnabled, setNotificationsEnabled] = useState(user.notificationsEnabled);
+  const [notificationsEnabled, setNotificationsEnabled] = useState(user.notificationsEnabled || false);
 
-  const { handleChange, handleSubmit } = useForm({
-    submitCallback: updateUser,
-    args: {
-      userId: user.id,
-    },
-  });
+  // const { handleChange, handleSubmit } = useForm({
+  //   submitCallback: updateUser,
+  //   args: {
+  //     userId: user.id,
+  //   },
+  // });
 
   const handleAll = async (e) => {
     setNotificationsEnabled(e.target.checked);
-    handleChange({ target: { name: e.target.name, value: e.target.checked }});
-    await handleSubmit(new Event('submit')); // Hack
+    updateUser({ userId: user.id, notificationsEnabled: e.target.checked });
   }
 
   // TODO: Add toggle instead of checkbox
@@ -26,7 +25,7 @@ const OtherConfigurationsForm = ({ user }) => {
   return (
     <div className="flex flex-col w-full gap-5">
       <h1 className="text-3xl">{t('pages.settings.otherConfigurations.title')}</h1>
-      <form onSubmit={handleSubmit} className="gap-5 w-full settings-form">
+      <form className="gap-5 w-full settings-form">
         <div className="settings-input-row w-full">
           <span className="font-bold">{t('pages.settings.otherConfigurations.receiveMails')}</span>
             <input type='checkbox' name="notificationsEnabled" onChange={handleAll} checked={notificationsEnabled}/>
