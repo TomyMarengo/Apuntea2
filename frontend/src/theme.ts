f3ae22fd8e83c23e0cdb10a8ba76bf353543af7b
TomyMarengo <marengotomas@gmail.com>
2025-01-03 15:08:22 -0300

    Added hover to buttons

--- frontend/src/theme.ts
+++ frontend/src/theme.ts
@@ -1,9 +1,7 @@
 // src/theme.ts
 
-import { PaletteColorOptions } from '@mui/material';
-import { createTheme } from '@mui/material/styles';
+import { PaletteColorOptions, createTheme, alpha } from '@mui/material/styles';
 
-// Extender la interfaz de Palette para incluir 'tertiary'
 declare module '@mui/material/styles' {
   interface Palette {
     tertiary: PaletteColorOptions;
@@ -11,65 +9,7 @@ declare module '@mui/material/styles' {
   interface PaletteOptions {
     tertiary?: PaletteColorOptions;
   }
-}
 
-export const lightTheme = createTheme({
-  palette: {
-    mode: 'light',
-    primary: {
-      main: '#EF7765',
-      dark: '#CC6252',
-    },
-    secondary: {
-      main: '#FDC286',
-      dark: '#FCAA6F',
-    },
-    text: {
-      primary: '#5A5044',
-      secondary: '#302A24',
-    },
-    background: {
-      default: '#FEFEFE',
-      paper: '#F9F1E7',
-    },
-  },
-  typography: {},
-  custom: {
-    '--gradient-top': '#F9F1E7',
-    '--gradient-mid': '#F8EED8',
-    '--gradient-bot': '#F7E4BA',
-  },
-});
-
-export const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-    primary: {
-      main: '#B3E8C3',
-      dark: '#7FB58F',
-    },
-    secondary: {
-      main: '#50C4EC',
-      dark: '#2F9CBB',
-    },
-    text: {
-      primary: '#FEFEFE',
-      secondary: '#F9F1E7',
-    },
-    background: {
-      default: '#454D55',
-      paper: '#0A0B10',
-    },
-  },
-  typography: {},
-  custom: {
-    '--gradient-top': '#0A0B10',
-    '--gradient-mid': '#0C0510',
-    '--gradient-bot': '#101214',
-  },
-});
-
-declare module '@mui/material/styles' {
   interface Theme {
     custom: {
       [key: string]: string;
@@ -81,3 +21,107 @@ declare module '@mui/material/styles' {
     };
   }
 }
+
+// Función para crear el tema base
+const createBaseTheme = (mode: 'light' | 'dark') => {
+  return createTheme({
+    palette: {
+      mode,
+      ...(mode === 'light'
+        ? {
+            primary: {
+              main: '#EF7765',
+              dark: '#CC6252',
+            },
+            secondary: {
+              main: '#FDC286',
+              dark: '#FCAA6F',
+            },
+            text: {
+              primary: '#5A5044',
+              secondary: '#302A24',
+            },
+            background: {
+              default: '#FEFEFE',
+              paper: '#F9F1E7',
+            },
+          }
+        : {
+            primary: {
+              main: '#B3E8C3',
+              dark: '#7FB58F',
+            },
+            secondary: {
+              main: '#50C4EC',
+              dark: '#2F9CBB',
+            },
+            text: {
+              primary: '#FEFEFE',
+              secondary: '#F9F1E7',
+            },
+            background: {
+              default: '#454D55',
+              paper: '#0A0B10',
+            },
+          }),
+    },
+    typography: {},
+    custom: {
+      ...(mode === 'light'
+        ? {
+            '--gradient-top': '#F9F1E7',
+            '--gradient-mid': '#F8EED8',
+            '--gradient-bot': '#F7E4BA',
+          }
+        : {
+            '--gradient-top': '#0A0B10',
+            '--gradient-mid': '#0C0510',
+            '--gradient-bot': '#101214',
+          }),
+    },
+  });
+};
+
+const lightBaseTheme = createBaseTheme('light');
+const darkBaseTheme = createBaseTheme('dark');
+
+const extendThemeWithOverrides = (
+  baseTheme: ReturnType<typeof createBaseTheme>,
+) => {
+  return createTheme(baseTheme, {
+    components: {
+      MuiButton: {
+        styleOverrides: {
+          root: {
+            transition: 'background-color 0.3s, color 0.3s',
+          },
+          contained: {
+            '&:hover': {
+              backgroundColor: baseTheme.palette.primary.dark,
+              color: baseTheme.palette.primary.contrastText,
+            },
+          },
+          outlined: {
+            '&:hover': {
+              backgroundColor: baseTheme.palette.primary.main,
+              color: baseTheme.palette.background.paper,
+            },
+          },
+        },
+      },
+      MuiIconButton: {
+        styleOverrides: {
+          root: {
+            transition: 'background-color 0.3s, color 0.3s',
+            '&:hover': {
+              backgroundColor: alpha(baseTheme.palette.text.primary, 0.1),
+            },
+          },
+        },
+      },
+    },
+  });
+};
+
+export const lightTheme = extendThemeWithOverrides(lightBaseTheme);
+export const darkTheme = extendThemeWithOverrides(darkBaseTheme);
